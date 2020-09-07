f97b13f0e72118f493784019d135f0078e89047e
Vanessa Wang <vanessa80332@gmail.com>
2020-09-07 16:50:10 +0800

    date style

--- src/components/Datetimepicker.jsx
+++ src/components/Datetimepicker.jsx
@@ -5,11 +5,10 @@ export default class Datetimepicker extends Component {
         super(props)
         this.state = {
             datetitle: ['日','一','二','三','四','五','六'],
-            today: new Date(),
             select: {
                 year: new Date().getFullYear(),
-                month: new Date().getMonth(),
-                day: new Date().getDay()
+                month: new Date().getMonth()+1,
+                date: new Date().getDate()
             },
             openYearMonth: false,
             openMonth: new Date().getFullYear(),
@@ -41,22 +40,49 @@ export default class Datetimepicker extends Component {
         })
     }
 
-    selectDay = (year,month,day,hour,min) => {
+    selectDay = (year,month,date,hour,min) => {
         this.setState({
             select:{
-                year: year==null? this.state.year: year,
-                month: month==null? this.state.month: month,
-                day: day==null? this.state.day: day,
-                hour: hour==null? this.state.hour: hour,
-                min: min==null? this.state.min: min,
+                year: year==null? this.state.select.year: year,
+                month: month==null? this.state.select.month: month,
+                date: date==null? this.state.select.date: date,
+                hour: hour==null? this.state.select.hour: hour,
+                min: min==null? this.state.select.min: min,
             }
         })
     }
 
+    renderDate = (y,month) => {
+        var m = month-1
+        var days = (new Date(y,m+1,1) - new Date(y,m,1))/(86400*1000)
+        var arr = []
+
+        if(new Date(y,m,1).getDay() != 0){
+            var lastdays = (new Date(y,m,1) - new Date(y,m-1,1))/(86400*1000)
+            for(var i=new Date(y,m,1).getDay()-1; i>=0; i--){
+                arr.push(lastdays-i)
+            }
+        }
+
+        for(var i=1; i<=days;i++){
+            arr.push(i)
+        }
+
+        if(new Date(y,m,days).getDay() != 6){
+            for(var i=1; i<7-new Date(y,m,days).getDay(); i++){
+                arr.push(i)
+            }
+        }
+        var week = []
+        for(var i=0; i<arr.length; i+=7){
+            week.push(arr.slice(i, i+7))
+        }
+        return week
+    }
 
 
     render() {
-        const { datetitle, today, openYearMonth, openMonth, month, select } = this.state
+        const { datetitle, openYearMonth, openMonth, month, select } = this.state
         const { options } = this.props
         return (
             <div>
@@ -90,11 +116,25 @@ export default class Datetimepicker extends Component {
                                 <div className="date">
                                     {
                                         datetitle.map((w, index) => 
-                                        <div className="datetitle" key={index}>{w}</div> 
+                                        <div className="datetitle" key={index}><span>{w}</span></div> 
                                         // (new Date(2020,10,1) - new Date(2020,9,1))/(86400*1000)-1
                                         )
                                     }
                                 </div>
+                                
+                                {
+                                    this.renderDate(select.year, select.month).map((week, index) => 
+                                        <div className="date" key={index}>
+                                            {
+                                                week.map((date,index) => 
+                                                    <div key={index} className={select.date==date?"date onclick select":"date onclick"} onClick={() => this.selectDay(null,null,date,null,null)}>
+                                                        <span>{date}</span>
+                                                    </div>
+                                                )
+                                            }
+                                        </div>
+                                    )
+                                }
                             </div>
                     }
                 </div>
